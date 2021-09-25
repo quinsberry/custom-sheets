a8e510385122d885a992feffef46f8e7d1d6768f
ekolesnyk <eugene.kolesnyk@customertimes.com>
2021-09-25 21:59:29 +0300

    - implemented new feature in DOM class

--- src/components/excel/Excel.ts
+++ src/components/excel/Excel.ts
@@ -2,7 +2,8 @@ import { Formula } from '@/components/formula/Formula';
 import { Toolbar } from '../toolbar/Toolbar';
 import { Table } from '@/components/table/Table';
 import { Header } from '@/components/header/Header';
-import { $ } from '@/core/dom';
+import { $, DOM } from '@/core/dom';
+import { assertNonNull } from '@/utils/type-guards';
 
 interface ExcelOptions {
     components: Component[];
@@ -10,25 +11,24 @@ interface ExcelOptions {
 
 type Component = typeof Formula | typeof Header | typeof Table | typeof Toolbar;
 export class Excel {
-    private $el: Element;
+    private $el: DOM;
     private components: Component[];
 
     constructor(selector: string, options: ExcelOptions) {
         const expectedElement = document.querySelector(selector);
-        if (!expectedElement) {
-            throw new Error(`DOM element not found by provided selector ${selector}`);
-        }
-        this.$el = expectedElement;
+        assertNonNull(expectedElement, `DOM element not found by provided selector ${selector}`);
+
+        this.$el = $(expectedElement);
         this.components = options?.components ?? [];
     }
 
-    getRoot() {
+    getRoot(): DOM {
         const $root = $.create('div', 'excel');
 
         this.components.forEach(Component => {
             const $el = $.create('div', Component.className);
-            const component = new Component($el);
-            $el.innerHTML = component.toHTML();
+            const component = new Component($el.$el);
+            $el.html(component.toHTML());
             $root.append($el);
         });
         return $root;
--- src/core/dom.ts
+++ src/core/dom.ts
@@ -1,17 +1,47 @@
-class DOM {
-    constructor() {
+import { assertNonNull } from '@/utils/type-guards';
 
+export class DOM {
+    $el: Element;
+
+    constructor(selector: string | Element) {
+        const expectedElement = typeof selector === 'string' ? document.querySelector(selector) : selector;
+        assertNonNull(expectedElement, `DOM element not found by provided selector ${selector}`);
+
+        this.$el = expectedElement;
+    }
+
+    html(html?: string): DOM | string {
+        if (typeof html === 'string') {
+            this.$el.innerHTML = html;
+            return this;
+        }
+        return this.$el.outerHTML.trim();
+    }
+
+    clear(): DOM {
+        this.html('');
+        return this;
+    }
+
+    on() {}
+
+    append(node: DOM | Element): DOM {
+        if (node instanceof DOM) {
+            node = node.$el;
+        }
+        this.$el.append(node);
+        return this;
     }
 }
 
-export function $() {
-    return new DOM();
+export function $(selector: string | Element) {
+    return new DOM(selector);
 }
 
-$.create = (tagName: string, classes = ''): HTMLElement => {
+$.create = (tagName: string, classes = ''): DOM => {
     const el = document.createElement(tagName);
     if (classes) {
         el.classList.add(classes);
     }
-    return el;
+    return $(el);
 };
